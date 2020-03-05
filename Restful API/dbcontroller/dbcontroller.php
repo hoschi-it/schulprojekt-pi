9b251222018eb966a04dcb2b89145c0b1d765d48
Dimi1991 <dimitri.khodak@continental.com>
2020-03-05 12:31:50 +0100

    Allgemein

--- Restful API/dbcontroller/dbcontroller.php
+++ Restful API/dbcontroller/dbcontroller.php
@@ -3,10 +3,10 @@
     // Press the 'Run' button on the top to start the web server,
     // then click the URL that is emitted to the Output tab of the console.
 
-    $servername = getenv('IP');
-    $username = getenv('C9_USER');
+    $servername = getenv('localhost');
+    $username = getenv('');
     $password = "";
-    $database = "c9";
+    $database = "networkmaintenance";
     $dbport = 3306;
 
     // Create connection
@@ -15,7 +15,7 @@
     // Check connection
     if ($db->connect_error) {
         die("Connection failed: " . $db->connect_error);
-    } 
+    }
     #echo "Connected successfully (".$db->host_info.")";
-    
+
 $con=$db;
